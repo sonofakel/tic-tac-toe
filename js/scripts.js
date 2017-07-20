dd153ec20e72a1064c55d6cbf37cb5d6a283e431
Guest User <Guest@epicodus-5.local>
2017-07-20 14:34:00 -0700

    commit for stash.

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,47 @@ function Player(name) {
   this.name = name;
 }
 
-Player.prototype.mark = function() {
-  if (this.name === "Player 1") {
-    return "x";
+function Game() {
+  this.player1 = new Player("Player1");
+  this.player2 = new Player("Player2");
+  this.currentPlayer = this.player1;
+  this.spaceMarked = false
+}
+
+Game.prototype.switchPlayers = function() {
+  if (this.currentPlayer.name === "Player1"){
+    this.currentPlayer = this.player2;
   } else {
-    return "o"
+    this.currentPlayer = this.player1;
   }
 }
 
 
+Game.prototype.mark = function() {
+  if (this.currentPlayer.name === "Player1") {
+    return "x";
+    this.switchPlayers;
+  } else {
+    return "o";
+  }
+}
 
 Game.prototype.marked = function() {
   if (this.spaceMarked === false) {
     this.spaceMarked = true;
-  } else {
-    this.spaceMarked= false;
   }
 }
 
-function Board() {
-  this.x = 1;
-  this.y = 2;
-}
 
-
-function Game() {
-  this.player1 = new Player("Player1");
-  this.player2 = new Player("Player2");
-  this.currentPlayer = this.Player1;
-  this.board =
-  this.spaceMarked = false;
-}
-
-Game.prototype.switchPlayers = function() {
-  if (this.currentPlayer === this.Player1){
-    this.currentPlayer = this.Player2;
-  } else {
-  this.currentPlayer = this.Player1;
-  }
-}
 
 var newGame = new Game();
 
 
 // UI Logic -->
+$(function(){
+
+  $(".space1").click(function(){
+    $(".space1").text(newGame.mark());
+
+  });
+});
